6a2a948553852495169650a993bbc78b78082689
theil <theil_27@hotmail.com>
2015-11-01 19:15:04 -0430

    Change bower install to the before_start section for travis.

---
+++ server/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    //return gulp.src('client/dist*')
+    //    .pipe(concat('main.min.css'))
+    //    .pipe(gulp.dest('dist/css'));
+    console.log("Hello");
+});
